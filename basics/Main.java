da89b91e1d3296e5a6af470f80d14dcfa6b63c11
naveena-kothamasu <naveenakothamasu@gmail.com>
2025-06-16 16:26:04 +0530

    Add files via upload

---
+++ basics/Main.java
@@ -0,0 +1,11 @@
+class Person { 
+    void sayHello() { 
+        System.out.println("Hello!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person(); 
+        p.sayHello();     }
+}
---
+++ basics/PrintNameFunction.java
@@ -0,0 +1,9 @@
+public class PrintNameFunction {
+    static void printMyName() { 
+        System.out.println("Naveena");
+    }
+
+    public static void main(String[] args) {
+        printMyName(); 
+    }
+}
